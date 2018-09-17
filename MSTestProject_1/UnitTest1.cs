b95d2e7196457c3a71b1b93367ecb773535907b0
Волков Сергей <volkovsn92@gmail.com>
2018-09-17 17:35:34 +0300

    mozilla added

--- MSTestProject_1/UnitTest1.cs
+++ MSTestProject_1/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
 
 namespace MSTestProject_1
 {
@@ -8,6 +11,10 @@ namespace MSTestProject_1
         [TestMethod]
         public void TestMethod1()
         {
+            IWebDriver driver = new FirefoxDriver(".");
+            driver.Navigate().GoToUrl("https://yandex.by");
+            Thread.Sleep(10000);
+            driver.Close();
             Assert.IsTrue(true, "Test failed.");
         }
     }
