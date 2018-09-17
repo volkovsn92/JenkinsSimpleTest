1d007d23e59a79777d31ed616795ed013779190c
Волков Сергей <volkovsn92@gmail.com>
2018-09-17 15:00:28 +0300

    first commit

---
+++ MSTestProject_1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestProject_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true, "Test failed.");
+        }
+    }
+}
